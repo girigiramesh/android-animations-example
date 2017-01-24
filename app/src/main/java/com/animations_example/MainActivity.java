d37083be05ec0d6cb76e037cabc91745f0761a63
Ramesh <ramesh.kvana@gmail.com>
2017-01-24 12:30:44 +0530

    auto transition sample is added

--- app/src/main/java/com/animations_example/MainActivity.java
+++ app/src/main/java/com/animations_example/MainActivity.java
@@ -1,13 +1,35 @@
 package com.animations_example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.transitionseverywhere.TransitionManager;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ViewGroup transitionsContainer = (ViewGroup) findViewById(R.id.transitions_container);
+        text = (TextView) transitionsContainer.findViewById(R.id.text);
+        button = (Button) transitionsContainer.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            boolean visible;
+
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(transitionsContainer);
+                visible = !visible;
+                text.setVisibility(visible ? View.VISIBLE : View.GONE);
+            }
+        });
     }
 }
