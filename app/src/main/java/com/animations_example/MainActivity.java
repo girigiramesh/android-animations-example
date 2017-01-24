61777488de4b2e2c6dcdff36f5c06fde29df1016
Ramesh <ramesh.kvana@gmail.com>
2017-01-24 13:33:17 +0530

    recolor and isrotated is added

--- app/src/main/java/com/animations_example/MainActivity.java
+++ app/src/main/java/com/animations_example/MainActivity.java
@@ -1,5 +1,6 @@
 package com.animations_example;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.view.animation.FastOutLinearInInterpolator;
 import android.support.v4.view.animation.LinearOutSlowInInterpolator;
@@ -7,16 +8,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.transitionseverywhere.Fade;
+import com.transitionseverywhere.Recolor;
+import com.transitionseverywhere.Rotate;
 import com.transitionseverywhere.TransitionManager;
 import com.transitionseverywhere.TransitionSet;
 import com.transitionseverywhere.extra.Scale;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iv_plus;
     private TextView text, tv_scale_fade;
-    Button button, button_scale_fade;
+    Button button, button_scale_fade, btn_recolor;
     ViewGroup transitionsContainer;
     boolean visible;
 
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         transitionsContainer = (ViewGroup) findViewById(R.id.transitions_container);
+//  -------- auto transition sample --------
         text = (TextView) transitionsContainer.findViewById(R.id.text);
         button = (Button) transitionsContainer.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 text.setVisibility(visible ? View.VISIBLE : View.GONE);
             }
         });
-
+//  ------- scale and fade sample ---------
         tv_scale_fade = (TextView) transitionsContainer.findViewById(R.id.tv_scale_fade);
         button_scale_fade = (Button) transitionsContainer.findViewById(R.id.button_scale_fade);
         button_scale_fade.setOnClickListener(new View.OnClickListener() {
@@ -45,12 +51,40 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 visible = !visible;
                 TransitionSet set = new TransitionSet()
-                        .addTransition(new Scale(0.7f))
+                        .addTransition(new Scale(0.1f))
                         .addTransition(new Fade())
                         .setInterpolator(visible ? new LinearOutSlowInInterpolator() : new FastOutLinearInInterpolator());
                 TransitionManager.beginDelayedTransition(transitionsContainer, set);
                 tv_scale_fade.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
             }
         });
+//  ---------- recolor sample -----------
+        btn_recolor = (Button) findViewById(R.id.btn_recolor);
+        btn_recolor.setOnClickListener(new View.OnClickListener() {
+            boolean isColorsInverted;
+            boolean mColorsInverted;
+
+            @Override
+            public void onClick(View view) {
+                isColorsInverted = !isColorsInverted;
+                TransitionManager.beginDelayedTransition(transitionsContainer, new Recolor());
+                btn_recolor.setTextColor(getResources().getColor(!isColorsInverted ? R.color.colorPrimary : R.color.colorAccent));
+                mColorsInverted = !mColorsInverted;
+                btn_recolor.setBackgroundDrawable(new ColorDrawable(getResources().getColor(!mColorsInverted ? R.color.colorAccent : R.color.colorPrimary)));
+            }
+        });
+//  ----------- isRotated sample ----------
+        iv_plus = (ImageView) findViewById(R.id.iv_plus);
+        iv_plus.setOnClickListener(new View.OnClickListener() {
+            boolean isRotated;
+
+            @Override
+            public void onClick(View view) {
+                isRotated = !isRotated;
+                TransitionManager.beginDelayedTransition(transitionsContainer, new Rotate());
+                iv_plus.setRotation(isRotated ? 135 : 0);
+            }
+        });
+
     }
 }
